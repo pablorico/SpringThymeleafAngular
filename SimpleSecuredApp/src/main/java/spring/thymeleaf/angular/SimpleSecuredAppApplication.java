e038f3c3b98299daac1a10e2ff006393a185e1cf
pablo <pablo@N207>
2017-06-10 21:16:12 -0300

    Just created

---
+++ SimpleSecuredApp/src/main/java/spring/thymeleaf/angular/SimpleSecuredAppApplication.java
@@ -0,0 +1,12 @@
+package spring.thymeleaf.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSecuredAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSecuredAppApplication.class, args);
+	}
+}
